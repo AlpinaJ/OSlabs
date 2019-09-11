d565c60d3c60972f28ba21f26fdf43a04853ebda
julia953197 <julia953197@yandex.ru>
2019-09-11 15:10:50 +0300

    Week 4.1

---
+++ week4/ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n=100;
+    int pid=fork();
+    if(pid != 0)
+		printf("hello from parent %d - %d\n", pid, n);
+	else{
+		printf("Hello from child %d - %d\n", pid,n);		
+	}
+    return 0;
+}
