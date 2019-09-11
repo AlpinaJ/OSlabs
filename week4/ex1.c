340328b7d8ffdc344e6ded5e052bce2c5ee9c0fa
julia953197 <julia953197@yandex.ru>
2019-09-11 15:14:13 +0300

    Week 4.1

--- week4/ex1.c
+++ week4/ex1.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+/* Explanation:
+    fork will return 0 for child processor and real PID of child processor for parent processor
+*/
 
-int main(){
-    int n=10;
-    int pid=fork();
-    if(pid != 0)
-		printf("hello from parent %d - %d\n", pid, n);
-	else{
-		printf("Hello from child %d - %d\n", pid,n);		
-	}
+int main()
+{
+    int n = 10;
+    int pid = fork();
+    if (pid != 0)
+        printf("hello from parent %d - %d\n", pid, n);
+    else
+    {
+        printf("Hello from child %d - %d\n", pid, n);
+    }
     return 0;
 }
